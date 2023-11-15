aaec395c6fc9ff36fa7a8b655bcb9ff2c61749aa
MichaelCordonnier <Michael.cordonnier@student.howest.be>
2023-11-15 12:31:52 +0100

    totaalNogNieKlaar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// use these elements to render the chart, legend and controls
 const $legendEl = document.querySelector(`.js-legend`);
 const $chartEl = document.querySelector(`.js-chart`);
 const $controlsEl = document.querySelector(`.js-controls`);
@@ -12,12 +11,15 @@ const cos = Math.cos;
 const sin = Math.sin;
 const π = Math.PI;
 
-const f_matrix_times = (([[a, b], [c, d]], [x, y]) => [a * x + b * y, c * x + d * y]);
-const f_rotate_matrix = (x => [[cos(x), -sin(x)], [sin(x), cos(x)]]);
-const f_vec_add = (([a1, a2], [b1, b2]) => [a1 + b1, a2 + b2]);
+const f_matrix_times = ([[a, b], [c, d]], [x, y]) => [a * x + b * y, c * x + d * y];
+const f_rotate_matrix = (x) => [
+  [cos(x), -sin(x)],
+  [sin(x), cos(x)],
+];
+const f_vec_add = ([a1, a2], [b1, b2]) => [a1 + b1, a2 + b2];
 
-const drawArc = (([cx, cy], [rx, ry], [t1, Δ], φ,) => {
-    /* [
+const drawArc = ([cx, cy], [rx, ry], [t1, Δ], φ) => {
+  /* [
     returns a SVG path element that represent a ellipse.
     cx,cy → center of ellipse
     rx,ry → major minor radius
@@ -28,21 +30,21 @@ const drawArc = (([cx, cy], [rx, ry], [t1, Δ], φ,) => {
     URL: SVG Circle Arc http://xahlee.info/js/svg_circle_arc.html
     Version 2019-06-19
      ] */
-    // convert t1 from degree to radian
-    t1 = t1 / 360 * 2 * π;
-    // convert Δ from degree to radian
-    Δ = Δ / 360 * 2 * π;
-    Δ = Δ % (2 * π);
-    // convert φ from degree to radian
-    φ = φ / 360 * 2 * π;
-    const rotMatrix = f_rotate_matrix(φ);
-    const [sX, sY] = (f_vec_add(f_matrix_times(rotMatrix, [rx * cos(t1), ry * sin(t1)]), [cx, cy]));
-    const [eX, eY] = (f_vec_add(f_matrix_times(rotMatrix, [rx * cos(t1 + Δ), ry * sin(t1 + Δ)]), [cx, cy]));
-    const fA = ((Δ > π) ? 1 : 0);
-    const fS = ((Δ > 0) ? 1 : 0);
-    const d = `M ${sX} ${sY} A ${[rx, ry, φ / (2 * π) * 360, fA, fS, eX, eY].join(" ")}`;
-    return d;
-});
+  // convert t1 from degree to radian
+  t1 = (t1 / 360) * 2 * π;
+  // convert Δ from degree to radian
+  Δ = (Δ / 360) * 2 * π;
+  Δ = Δ % (2 * π);
+  // convert φ from degree to radian
+  φ = (φ / 360) * 2 * π;
+  const rotMatrix = f_rotate_matrix(φ);
+  const [sX, sY] = f_vec_add(f_matrix_times(rotMatrix, [rx * cos(t1), ry * sin(t1)]), [cx, cy]);
+  const [eX, eY] = f_vec_add(f_matrix_times(rotMatrix, [rx * cos(t1 + Δ), ry * sin(t1 + Δ)]), [cx, cy]);
+  const fA = Δ > π ? 1 : 0;
+  const fS = Δ > 0 ? 1 : 0;
+  const d = `M ${sX} ${sY} A ${[rx, ry, (φ / (2 * π)) * 360, fA, fS, eX, eY].join(" ")}`;
+  return d;
+};
 
 // get the data with async and start the chain of functions
 // pass the data to the next function in the chain
@@ -64,10 +66,48 @@ const drawArc = (([cx, cy], [rx, ry], [t1, Δ], φ,) => {
 // the theme toggle should remember your choice in localStorage
 // and it should sync with system changes
 
-// init function
+const fetchLocalJSON = async () => {
+  try {
+    const response = await fetch("assets/data/data.json");
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
-const init = () => {
+const renderStaticHtml = (data) => {
+  // render the radiobtns
+  renderRdBtns(data);
+  //   render the legends
+  renderLegends(data);
+};
 
+const renderRdBtns = (data) => {
+  for (let party in data) {
+    // render a radio button and a label for each party
+    $controlsEl.innerHTML += '<div class="button-control"><input type="radio" name="party" id="' + party + '" value="' + party + '" class="sr-only"><label for="' + party + '">' + party + "</label></div>";
+  }
+};
+
+const renderLegends = (data) => {
+  let html = "";
+  for (let party in data) {
+    html += '<ul class="legend ' + party + '">';
+    let counter = 0;
+    for (let ageGroup in data[party].ages) {
+      html += '<li style="--legendColor: var(--color-' + counter + ')" class="legend_item"><span class="legend__key">' + ageGroup + '</span><span class="legend__value">' + data[party].ages[ageGroup] + "</span></li>";
+      counter++;
+    }
+    html += "</ul>";
+  }
+  $legendEl.innerHTML = html;
+};
+
+const init = async () => {
+  const data = await fetchLocalJSON();
+  renderStaticHtml(data);
 };
 
 init();
